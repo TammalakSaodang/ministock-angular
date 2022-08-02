80f529bbb9ecffe0454c499931052034408100a9
Tammalak <tammalak.s@gmail.com>
2022-08-02 22:49:13 +0700

    update ver1 ang ministock

--- src/app/pages/backend/dashboard/dashboard.component.ts
+++ src/app/pages/backend/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService  } from "../../../services/product.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  //สร้างตัวแปนมารับค่าจาก API
+  dataProduct:any = [] //any คือชนิดตัวแปรถ้าเราไม่รู้ type ไม่ควรใช้บ่อยๆ เพราะมันกิน Memory
+
+  constructor(public api: ProductService) { }  //public เพราะว่าให้ใครเรียกก็ได้
 
   ngOnInit(): void {
+    this.api.getProducts().subscribe((data:{}) =>{
+        console.log(data);
+        this.dataProduct = data ;
+    })
   }
 
 }
--- src/app/pages/login-register/login-register.component.ts
+++ src/app/pages/login-register/login-register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router} from '@angular/router';
 @Component({
   selector: 'app-login-register',
   templateUrl: './login-register.component.html',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginRegisterComponent implements OnInit {
 
-  constructor() { }
+  //1 way data binding
+  message:string = "Hello Angular";
+  //variable Object
+  profile = {
+    "name":"Tammalak.S" ,
+    "tel":"099" ,
+    "gender":"male"
+  } 
+  //2 way data binding
+  userData = {
+    "email":"" ,
+    "password":""
+  }
+
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  submitLogin(){
+    //alert("Hey Angular");
+    if (this.userData.email == "admin@email.com" && this.userData.password == "1234") {
+      //alert("Login success");
+      this.router.navigate(['backend']);
+    }
+    else{
+      alert("Login Fail !!!");
+    }
+  }
+  
 }
